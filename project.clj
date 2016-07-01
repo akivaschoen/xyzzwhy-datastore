1ef768f2ce8b3eaf4f62fe642c11c544937367ad
Akiva <akiva@akivamail.com>
2016-07-01 09:29:45 -0500

    Updates dependencies.

--- project.clj
+++ project.clj
@@ -8,24 +8,31 @@
 
   :dependencies
   [[org.clojure/clojure "1.8.0"]
-   [com.apa512/rethinkdb "0.15.12"]
-   [environ "1.0.2"]
+   [com.apa512/rethinkdb "0.15.24"]
+   [environ "1.0.3"]
    [pluralex "1.0.0-SNAPSHOT"]]
 
   :plugins
-  [[lein-autoexpect "1.7.0"]
-   [lein-environ "1.0.1"]
-   [lein-expectations "0.0.8"]]
+  [[lein-environ "1.0.3"]]
 
   :profiles
   {:uberjar
    [:prod-config
-    {:env {:production true}
+    {:env {:production "true"}
      :omit-source true
      :aot :all}]
 
    :dev
-   {:env {:dev true}
-    :plugins []
-    :dependencies [[expectations "2.1.4"]]}})
+    {:env {:dev "true"}
+     :source-paths ["dev"]
+     :dependencies [[pjstadig/humane-test-output "0.8.0"]
+                    [leiningen "2.6.1"]]
+     :plugins [[com.jakemccrary/lein-test-refresh "0.16.0"]]
+     :injections [(require 'pjstadig.humane-test-output)
+                  (pjstadig.humane-test-output/activate!)]
+     :test-refresh {:notify-command ["terminal-notifier" "-title" "Tests" "-message"]
+                    :quiet true
+                    :changes-only true}}}
 
+  :repl-options
+  {:caught clj-stacktrace.repl/pst+})
