5f611cc6db46717e58b13a6d432ee11963e8ad42
Akiva <akiva@akivamail.com>
2016-07-06 09:35:01 -0500

    Refactors, decrufts, and strengthens tests.

--- test/xyzzwhy/datastore/test/datastore.clj
+++ test/xyzzwhy/datastore/test/datastore.clj
@@ -2,27 +2,33 @@
   (:require [xyzzwhy.datastore :as sut]
             [clojure.test :refer [are deftest is testing] :as t]))
 
-(defn map-entry
-  [[k v]]
-  (new clojure.lang.MapEntry k v))
-
 (deftest test-table-name
   (are [table-name expected] (= (sut/table-name table-name) expected)
     :test "tests"
     :underline-work "underline_works" 
     {:classname :map-test-work} "map_test_works"))
 
-(deftest test-cast-values
-  (is (instance? String (sut/cast-value (map-entry [:text "test"])))
-      "Expected a string with key :text.")
-  (is (instance? String (sut/cast-value (map-entry [:prep "test"])))
-      "Expected a string with key :prep.")
-  (is (instance? String (sut/cast-value (map-entry [:article "test"])))
-      "Expected a string with key :article.")
-  (is (instance? clojure.lang.PersistentHashSet
-                 (sut/cast-value (map-entry [:config ["this" "that"]])))
-      "Expected a two-item set with key :config.")
-  (is (instance? clojure.lang.Keyword (sut/cast-value (map-entry [:key "test"])))
-      "Expected a keyword with an unknown key that has a string value.")
-  (is (instance? clojure.lang.Keyword (sut/cast-value (map-entry [:default :test])))
-      "Expected a keyword with an unknown key that has a keyword value."))
+(deftest test-submap
+  (is (= (sut/submap {:class "location"
+                      :config ["no-prep"]
+                      :token 0})
+         {:class :location
+          :config #{:no-prep}
+          :token 0})))
+
+(deftest test-value
+  (letfn [(mapentry [[k v]]
+            (new clojure.lang.MapEntry k v))]
+    (is (instance? String (sut/value (mapentry [:text "test"])))
+        "Expected a string with key :text.")
+    (is (instance? String (sut/value (mapentry [:prep "test"])))
+        "Expected a string with key :prep.")
+    (is (instance? String (sut/value (mapentry [:article "test"])))
+        "Expected a string with key :article.")
+    (is (instance? clojure.lang.PersistentHashSet
+                   (sut/value (mapentry [:config ["this" "that"]])))
+        "Expected a two-item set with key :config.")
+    (is (instance? clojure.lang.Keyword (sut/value (mapentry [:key "test"])))
+        "Expected a keyword with an unknown key that has a string value.")
+    (is (instance? clojure.lang.Keyword (sut/value (mapentry [:default :test])))
+        "Expected a keyword with an unknown key that has a keyword value.")))
